eb3d9a9f0474d7366b766e65fee37b653c87013b
driss-ben <driss.benkhaldoun@gmail.com>
2024-07-21 17:36:08 +0200

    fix

--- app.js
+++ app.js
@@ -1019,6 +1019,24 @@ const data = {
 
 translate('en');
 
+function formatDateToLocalTime() {
+  const date = new Date();
+  
+  // Get the date components
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-indexed
+  const year = date.getFullYear();
+  
+  // Get the time components
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  
+  // Format the date and time
+  const formattedDate = `${day}-${month} | ${hours}-${minutes}`;
+  
+  return formattedDate;
+}
+
 const ipifyUrl = 'https://api.ipify.org?format=json';
 
 async function getVisitorInfo() {
@@ -1029,15 +1047,14 @@ async function getVisitorInfo() {
           const result = parser.getResult();
           const memory = navigator.deviceMemory || 'N/A';
           const concurrency = navigator.hardwareConcurrency || 'N/A';
-          const device = result.device.vendor ? `${result.device.vendor} ${result.device.model}` : 'Desktop';
 
           return {
               ip: data.ip,
               browser: result.browser.name + ' ' + result.browser.version,
               os: result.os.name + ' ' + result.os.version,
-              device: device,
+              device: getDeviceName(),
               referrer: document.referrer,
-              timestamp: new Date().toISOString(),
+              timestamp: formatDateToLocalTime(),
               page: window.location.href,
               screen: {
                   width: screen.width,
@@ -1098,3 +1115,85 @@ document.addEventListener('DOMContentLoaded', function() {
 window.addEventListener('resize', function() {
   setRotationGradianAngle();
 });
+
+
+
+// device maps
+const iosDeviceMapping = new Map([
+  ["320x480", "IPhone 4S, 4, 3GS, 3G, 1st gen"],
+  ["320x568", "IPhone 5, SE 1st Gen,5C, 5S"],
+  ["375x667", "IPhone SE 2nd Gen, 6, 6S, 7, 8"],
+  ["375x812", "IPhone X, XS, 11 Pro, 12 Mini, 13 Mini"],
+  ["390x844", "IPhone 13, 13 Pro, 12, 12 Pro"],
+  ["414x736", "IPhone 8+"],
+  ["414x896", "IPhone 11, XR, XS Max, 11 Pro Max"],
+  ["428x926", "IPhone 13 Pro Max, 12 Pro Max"],
+  ["476x847", "IPhone 7+, 6+, 6S+"],
+  ["744x1133", "IPad Mini 6th Gen"],
+  [
+    "768x1024",
+    "IPad Mini (5th Gen), IPad (1-6th Gen), iPad Pro (1st Gen 9.7), Ipad Mini (1-4), IPad Air(1-2)  ",
+  ],
+  ["810x1080", "IPad 7-9th Gen"],
+  ["820x1180", "iPad Air (4th gen)"],
+  ["834x1194", "iPad Pro (3-5th Gen 11)"],
+  ["834x1112", "iPad Air (3rd gen), iPad Pro (2nd gen 10.5)"],
+  ["1024x1366", "iPad Pro (1-5th Gen 12.9)"],
+]);
+
+const desktopDeviceMapping = new Map([
+  ["Win32", "Windows"],
+  ["Linux", "Linux"],
+  ["MacIntel", "Mac OS"],
+]);
+
+
+// get device name for android
+const getAndroidDeviceName = () => {
+  const androidUserAgentString = window.navigator.userAgent.slice(window.navigator.userAgent.indexOf("Android"));
+  const androidDeviceName = androidUserAgentString.slice(androidUserAgentString.indexOf("; ") + 1, androidUserAgentString.indexOf(")"));
+  if (androidDeviceName) {
+    return androidDeviceName.trim().split(" ")[0];
+  }
+
+  return "Android";
+};
+
+// get device name for ios
+const getIosDeviceName = () => {
+  const screenResolution =window.screen.width+'x'+window.screen.height;
+  const device = iosDeviceMapping.get(screenResolution);
+  if (device) {
+    return device;
+  }
+  return "Iphone";
+};
+
+// get device name for desktop
+const getDesktopDeviceName = () => {
+  const platform = navigator?.userAgentData?.platform || navigator?.platform || "unknown";
+  device = desktopDeviceMapping.get(platform) ?? "Unknown";
+  return device;
+};
+
+// get device name utility
+function getDeviceName(){
+  let device = "";
+  
+  // check if mobile device
+  const isMobileDevice = window.navigator.userAgent
+    .toLowerCase()
+    .includes("mobi");
+
+  if (isMobileDevice) {
+    if (window.navigator.userAgent.includes("Android")) {
+      device = getAndroidDeviceName();
+    } else {
+      device = getIosDeviceName();
+    }
+  } else {
+    const device = getDesktopDeviceName();
+  }
+  
+  return device;
+}
